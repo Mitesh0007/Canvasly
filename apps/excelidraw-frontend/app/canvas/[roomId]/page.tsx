add0d9b038a438459dcbaf7cdd1f130bfc2d7b1a
mitesh <miteshgupta00704@gmail.com>
2026-08-14 20:13:47 +0530

    Fix promise

--- apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,12 +1,11 @@
 import { RoomCanvas } from "@/components/RoomCanvas";
 
 export default async function CanvasPage({ params }: {
-    params: {
-        roomId: string
-    }
+  params: Promise<{
+    roomId: string
+  }>
 }) {
-    const roomId = (await params).roomId;
+  const roomId = (await params).roomId;
 
-    return <RoomCanvas roomId={roomId} />
-   
+  return <RoomCanvas roomId={roomId} />
 }
